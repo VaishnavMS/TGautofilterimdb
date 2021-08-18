334fc01aa77e7647b407d04f38dc93919d233375
Vaishnav <66957152+VaishnavMS@users.noreply.github.com>
2021-08-18 12:39:49 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -60,11 +60,7 @@ async def auto_filter(bot, update):
 
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!additional imdb plugin starts here;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
-    
-    
-    # -*- coding: utf-8 -*-
-
-
+  
 import telebot # Librería de la API del bot.
 from telebot import types # Tipos para la API del bot.
 import time # Librería para hacer que el programa que controla el bot no se acabe.
@@ -93,212 +89,9 @@ def obtener(m,namex,cid):
 
     lol = json.loads(parsed_data)
 
-    if lol["Response"]=='True':
-        if lol["Poster"] != "N/A":
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            urlpic = (lol["Poster"])    
-            urllib.urlretrieve(urlpic, "1.jpg")
-            file = open('1.jpg', 'rb')
-            bot.send_photo(cid,file,caption=caption)
-        else:
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            bot.send_message(cid,caption)
-    else:
-        bot.reply_to(m,'Content not found in IMDb!')
 
 
  
-@bot.message_handler(commands=['score']) 
-def command_score(m):  
-    cid = m.chat.id
-    obtener(m,m.text,cid)
- 
-@bot.message_handler(commands=['start']) 
-def command_bisi(m): # Definimos una función que resuelva lo que necesitemos.
-    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-    bot.send_message(cid, "Use the command /score and the name of the film/serie in order to get the score")
-
-bot.polling(none_stop=True)
-while True: 
-    time.sleep(300) # Hacemos que duerma durante un periodo largo de tiempo para que la CPU no esté trabajando innecesáremente.
-# -*- coding: utf-8 -*-
-
-
-import telebot # Librería de la API del bot.
-from telebot import types # Tipos para la API del bot.
-import time # Librería para hacer que el programa que controla el bot no se acabe.
-
-import json
-import requests
-import urllib
-from key import *
-
-bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
-
-def obtener(m,namex,cid):
-    boolxd = namex[:21] == '/score@imdb_score_bot'
-    
-    if boolxd == True:
-        name = namex[22:]
-       
-    else:
-        name = namex[7:]
-        
-    url = 'http://www.omdbapi.com/?t=' + name 
-    r = requests.get(url)
-
-    json_object = r.json()
-    parsed_data = json.dumps(json_object)
-
-    lol = json.loads(parsed_data)
-
-    if lol["Response"]=='True':
-        if lol["Poster"] != "N/A":
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            urlpic = (lol["Poster"])    
-            urllib.urlretrieve(urlpic, "1.jpg")
-            file = open('1.jpg', 'rb')
-            bot.send_photo(cid,file,caption=caption)
-        else:
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            bot.send_message(cid,caption)
-    else:
-        bot.reply_to(m,'Content not found in IMDb!')
-
-
- 
-@bot.message_handler(commands=['score']) 
-def command_score(m):  
-    cid = m.chat.id
-    obtener(m,m.text,cid)
- 
-@bot.message_handler(commands=['start']) 
-def command_bisi(m): # Definimos una función que resuelva lo que necesitemos.
-    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-    bot.send_message(cid, "Use the command /score and the name of the film/serie in order to get the score")
-
-bot.polling(none_stop=True)
-while True: 
-    time.sleep(300) # Hacemos que duerma durante un periodo largo de tiempo para que la CPU no esté trabajando innecesáremente.
-# -*- coding: utf-8 -*-
-
-
-import telebot # Librería de la API del bot.
-from telebot import types # Tipos para la API del bot.
-import time # Librería para hacer que el programa que controla el bot no se acabe.
-
-import json
-import requests
-import urllib
-from key import *
-
-bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
-
-def obtener(m,namex,cid):
-    boolxd = namex[:21] == '/score@imdb_score_bot'
-    
-    if boolxd == True:
-        name = namex[22:]
-       
-    else:
-        name = namex[7:]
-        
-    url = 'http://www.omdbapi.com/?t=' + name 
-    r = requests.get(url)
-
-    json_object = r.json()
-    parsed_data = json.dumps(json_object)
-
-    lol = json.loads(parsed_data)
-
-    if lol["Response"]=='True':
-        if lol["Poster"] != "N/A":
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            urlpic = (lol["Poster"])    
-            urllib.urlretrieve(urlpic, "1.jpg")
-            file = open('1.jpg', 'rb')
-            bot.send_photo(cid,file,caption=caption)
-        else:
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            bot.send_message(cid,caption)
-    else:
-        bot.reply_to(m,'Content not found in IMDb!')
-
-
- 
-@bot.message_handler(commands=['score']) 
-def command_score(m):  
-    cid = m.chat.id
-    obtener(m,m.text,cid)
- 
-@bot.message_handler(commands=['start']) 
-def command_bisi(m): # Definimos una función que resuelva lo que necesitemos.
-    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-    bot.send_message(cid, "Use the command /score and the name of the film/serie in order to get the score")
-
-bot.polling(none_stop=True)
-while True: 
-    time.sleep(300) # Hacemos que duerma durante un periodo largo de tiempo para que la CPU no esté trabajando innecesáremente.
-# -*- coding: utf-8 -*-
-
-
-import telebot # Librería de la API del bot.
-from telebot import types # Tipos para la API del bot.
-import time # Librería para hacer que el programa que controla el bot no se acabe.
-
-import json
-import requests
-import urllib
-from key import *
-
-bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
-
-def obtener(m,namex,cid):
-    boolxd = namex[:21] == '/score@imdb_score_bot'
-    
-    if boolxd == True:
-        name = namex[22:]
-       
-    else:
-        name = namex[7:]
-        
-    url = 'http://www.omdbapi.com/?t=' + name 
-    r = requests.get(url)
-
-    json_object = r.json()
-    parsed_data = json.dumps(json_object)
-
-    lol = json.loads(parsed_data)
-
-    if lol["Response"]=='True':
-        if lol["Poster"] != "N/A":
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            urlpic = (lol["Poster"])    
-            urllib.urlretrieve(urlpic, "1.jpg")
-            file = open('1.jpg', 'rb')
-            bot.send_photo(cid,file,caption=caption)
-        else:
-            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
-            bot.send_message(cid,caption)
-    else:
-        bot.reply_to(m,'Content not found in IMDb!')
-
-
- 
-@bot.message_handler(commands=['score']) 
-def command_score(m):  
-    cid = m.chat.id
-    obtener(m,m.text,cid)
- 
-@bot.message_handler(commands=['start']) 
-def command_bisi(m): # Definimos una función que resuelva lo que necesitemos.
-    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
-    bot.send_message(cid, "Use the command /score and the name of the film/serie in order to get the score")
-
-bot.polling(none_stop=True)
-while True: 
-    time.sleep(300) # Hacemos que duerma durante un periodo largo de tiempo para que la CPU no esté trabajando innecesáremente.
-
     
     
    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!imdb plugin ends here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -311,7 +104,22 @@ while True:
             file_type = filter.get("file_type")
             file_link = filter.get("file_link")
             file_size = int(filter.get("file_size", "0"))
-            
+            #!!!!!!!!!!!!!!!!!iMDB here also!!!!!!!!!!!!!!!
+              
+                if lol["file_name"]=='True':
+        if lol["Poster"] != "N/A":
+            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
+            urlpic = (lol["Poster"])    
+            urllib.urlretrieve(urlpic, "1.jpg")
+            file = open('1.jpg', 'rb')
+            bot.send_photo(cid,file,caption=caption)
+        else:
+            caption = 'Title: ' + (lol["Title"]) + '\n' +  '\n' + 'IMDb:  ' + (lol["imdbRating"]) + '/10   (' + lol["imdbVotes"] + ' votes)'  + '\n' + 'Metacritic:  ' + (lol["Metascore"])
+            bot.send_message(cid,caption)
+    else:
+        bot.reply_to(m,'Content not found in IMDb!')
+
+        
             # from B to MiB
             
             if file_size < 1024:
